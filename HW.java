025a08efa35ae0ff13342c4804aa4737d9dcbc85
berfinortulu <berfin.ortulu@ug.bilkent.edu.tr>
2023-09-20 15:48:23 +0300

    part d is finished

--- HW.java
+++ HW.java
@@ -42,4 +42,15 @@ public class HW {
 
         return min;
     }
+
+
+    public int averageFinder(int[] array){
+        int average = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            average += array[i];
+        }
+
+        return average / array.length;
+    }
 }
